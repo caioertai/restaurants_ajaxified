647102998100a59f4927cfc2f5a8d63e245dd9d3
Caio Andrade <caioertai@gmail.com>
2018-08-17 10:27:09 -0300

    Final touches

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      respond_to do |format|
+        format.html { redirect_to restaurant_path(@restaurant) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'restaurants/show' }
+        format.js  # <-- idem
+      end
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [ :index, :show ] do
+  resources :restaurants, only: [ :index, :show, :destroy ] do
     # /restaurants/1/reviews
     resources :reviews, only: [:create]
   end
+  resources :reviews, only: [:destroy]
+
   root to: 'restaurants#index'
 end
